f2476acf1a9ffab27e8467702608d25793c1151f
shrujanv <shrujanvenky@gmail.com>
2025-03-13 05:49:07 -0400

    First commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World I am Shrujan\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this, Yayyyy!! \n";
   return 0;
 }
